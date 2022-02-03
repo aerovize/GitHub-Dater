90c6a60ddd0626b7d641dcbc5ed9674d0e718e9b
Adam Weisend <maniacus.films@gmail.com>
2022-02-02 22:18:58 -0500

    Added error handling

--- ghub.py
+++ ghub.py
@@ -18,18 +18,28 @@ class Github:
         self.header = {"accept": "application/vnd.github.v3+json"}
 
     def get_repo(self, repo_name):
-        response = requests.get(
-            f"{self.url}/repos/{self.USERNAME}/{repo_name}", auth=(self.USERNAME, self.TOKEN), headers=self.header)
-        return response.json()
+        # Fetches a single repository
+        try:
+            response = requests.get(
+                f"{self.url}/repos/{self.USERNAME}/{repo_name}", auth=(self.USERNAME, self.TOKEN), headers=self.header)
+            return response.json()
+        except requests.ConnectionError as err:
+            return f"A error has occured trying to connect to the repository: {repo_name}, Error: {repr(err)}"
 
     def get_repo_content(self, repo_name, path):
+        # Fetches file content from a single repository
         repos = f"/repos/{self.USERNAME}/{repo_name}/contents/{path}"
-
-        response = requests.get(
-            f"{self.url}{repos}", auth=(self.USERNAME, self.TOKEN), headers=self.header)
-        return response.json()
+        try:
+            response = requests.get(
+                f"{self.url}{repos}", auth=(self.USERNAME, self.TOKEN), headers=self.header)
+            return response.json()
+        except requests.ConnectionError as err:
+            return f"A error has occured trying to connect to: {repo_name}/{path}, Error: {repr(err)}"
 
     def create_file(self, repo_name, commit_message, content, path):
+
+        # Creates a new file in a repository
+
         content = content.encode("utf-8")
         content = base64.b64encode(content).decode("utf-8")
 
@@ -37,22 +47,29 @@ class Github:
             "message": commit_message,
             "content": content,
         }
-
-        resp = requests.put(
-            f"{self.url}/repos/{self.USERNAME}/{repo_name}/contents/{path}", auth=(self.USERNAME, self.TOKEN), headers=self.header, json=body)
-        return resp.json()
+        try:
+            response = requests.put(
+                f"{self.url}/repos/{self.USERNAME}/{repo_name}/contents/{path}", auth=(self.USERNAME, self.TOKEN), headers=self.header, json=body)
+            return response.json()
+        except requests.ConnectionError as err:
+            return f"A error has occured trying to add the file: {path} to the repository: {repo_name}, Error: {err}"
 
     def create_repository(self, repo_name):
+        # Creates a new repository
         body = {
             "name": repo_name,
             "private": True,
         }
-
-        resp = requests.post(f"{self.url}/user/repos",
-                             auth=(self.USERNAME, self.TOKEN), headers=self.header, json=body)
-        return resp.json()
+        try:
+            resp = requests.post(f"{self.url}/user/repos",
+                                 auth=(self.USERNAME, self.TOKEN), headers=self.header, json=body)
+            return resp.json()
+        except requests.ConnectionError as err:
+            return f"A error has occured trying to create: {repo_name}, Error: {err}"
 
     def update_file(self, repo_name, commit_message, content, sha, path):
+        # Updates a file in a repository
+        # requires from the API, the sha value for the file
         content = content.encode("utf-8")
         content = base64.b64encode(content).decode("utf-8")
 
@@ -61,7 +78,9 @@ class Github:
             "content": content,
             "sha": sha
         }
-
-        resp = requests.put(
-            f"{self.url}/repos/{self.USERNAME}/{repo_name}/contents/{path}", auth=(self.USERNAME, self.TOKEN), headers=self.header, json=body)
-        return resp.json()
+        try:
+            resp = requests.put(
+                f"{self.url}/repos/{self.USERNAME}/{repo_name}/contents/{path}", auth=(self.USERNAME, self.TOKEN), headers=self.header, json=body)
+            return resp.json()
+        except requests.ConnectionError as err:
+            return f"A error has occured trying to update: {path} in the repo {repo_name}, Error: {err}"
