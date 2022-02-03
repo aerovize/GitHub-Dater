261e38e391c71bdad0b7cd998a043aa13cb05b19
Adam Weisend <maniacus.films@gmail.com>
2022-02-03 00:08:47 -0500

    refactor for date format

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ import time
 
 def getDate():
     dt = datetime.now()
-    datestamp = dt.strftime("%Y-%m-%d")
+    datestamp = dt.strftime("%m-%d-%Y")
     return datestamp
 
 
@@ -25,8 +25,7 @@ def make_request():
 
 - :wrench: My skills: HTML, CSS, Javascript, Nodejs, Python, & Web Security.
 - :bulb: Tech Interests: Security, Automation, and Backend Development.
-- 🔭 I’m currently working on: Obtaining my first developer role and becoming a better one.
-- 🌱 I’m currently learning: Rust
+- 🌱 I’m currently learning: Go
 - ⚡ Fun fact: I've previously worked as an underground coal miner and commercial truck driver.
 - :computer: [My Linkedin](https://www.linkedin.com/in/aweisend)"""
 
